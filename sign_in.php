b21e1730e10dd654fb04258768042721a94acadf
AV_Ivanov <90529554+cufrosso@users.noreply.github.com>
2022-10-17 13:54:33 +0300

    Update sign_in.php

--- sign_in.php
+++ sign_in.php
@@ -4,7 +4,7 @@
     require_once 'connect.php';
 
     $login = $_POST['login'];
-    $password = $_POST['password']; //md5
+    $password = $_POST['password'];
 
     $check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login'");
     if (mysqli_num_rows($check_user) === 1)
@@ -12,13 +12,21 @@
         $user = mysqli_fetch_assoc($check_user);
         if(password_verify($password, $user['password']))
         {
-        $_SESSION['user'] =
-            [
-                "id" => $user['id'],
-                "email" => $user['email'],
-                "login" => $user['login']
-            ];
-            header('Location: ../profile.php');
+            if (!empty($login) && !empty($email) && !empty($password) && !empty($password_confirm) && !empty($secret_word))
+            {
+                $_SESSION['user'] =
+                    [
+                        "id" => $user['id'],
+                        "email" => $user['email'],
+                        "login" => $user['login']
+                    ];
+                header('Location: ../profile.php');
+            }
+            else
+            {
+                $_SESSION['message'] = "empty field";
+                header('Location: ../login.php');
+            }
         }
         else
         {
