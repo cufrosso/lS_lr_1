a3f73bd8f97afee1972f88b9860fb73f7c07e94c
AV_Ivanov <90529554+cufrosso@users.noreply.github.com>
2022-10-24 14:03:44 +0300

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -11,25 +11,38 @@
     $check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login'");
     if (mysqli_num_rows($check_user) === 0)
     {
-        if ($password === $password_confirm)
+        $number = preg_match('@[0-9]@', $password);
+        $uppercase = preg_match('@[A-Z]@', $password);
+        $lowercase = preg_match('@[a-z]@', $password);
+        $specialChars = preg_match('@[^\w]@', $password);
+        if(strlen($password) < 4 || !$number || !$uppercase || !$lowercase || !$specialChars)
         {
-            $password_hsh = password_hash($password, PASSWORD_BCRYPT);
-            $secret_word_hsh = password_hash($secret_word, PASSWORD_BCRYPT);
-            if (!empty($login) && !empty($email) && !empty($password) && !empty($password_confirm) && !empty($secret_word))
-            {
-                mysqli_query($connect, "INSERT INTO `users` (`id`, `login`, `email`, `password`,`secret_word`) VALUES (NULL, '$login', '$email', '$password_hsh','$secret_word_hsh')");
-                header('Location: ../login.php');
-            }
-            else
-            {
-                $_SESSION['message'] = "empty field";
-                header('Location: ../signup.php');
-            }
+            $_SESSION['message'] = "password must be at least 8 characters in length and must contain at least one number, one upper case letter, one lower case letter and one special character";
+            header('Location: ../signup.php');
         }
         else
         {
-            $_SESSION['message'] = "passwords don't match";
-            header('Location: ../signup.php');
+            if ($password === $password_confirm)
+            {
+                $password_hsh = password_hash($password, PASSWORD_BCRYPT);
+                $secret_word_hsh = password_hash($secret_word, PASSWORD_BCRYPT);
+                if (!empty($login) && !empty($email) && !empty($password) && !empty($password_confirm) && !empty($secret_word))
+                {
+                    mysqli_query($connect, "INSERT INTO `users` (`id`, `login`, `email`, `password`,`secret_word`) VALUES (NULL, '$login', '$email', '$password_hsh','$secret_word_hsh')");
+                    header('Location: ../login.php');
+                    $_SESSION['message'] = "your password is strong";
+                }
+                else
+                {
+                    $_SESSION['message'] = "empty field";
+                    header('Location: ../signup.php');
+                }
+            }
+            else
+            {
+                $_SESSION['message'] = "passwords don't match";
+                header('Location: ../signup.php');
+            }
         }
     }
     else
