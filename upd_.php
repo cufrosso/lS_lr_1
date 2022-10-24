e7ae8f69dbac09661034b9a5d7cfff59be9870de
AV_Ivanov <90529554+cufrosso@users.noreply.github.com>
2022-10-24 14:09:10 +0300

    Update upd_.php

--- upd_.php
+++ upd_.php
@@ -1,5 +1,4 @@
 <?php
-    #ob_start();
     session_start();
     require_once 'connect.php';
 
@@ -11,46 +10,58 @@
     $login = mysqli_real_escape_string($connect,$_POST['login']);
     $sql = mysqli_query($connect,"SELECT * FROM `users` WHERE `login`='$login' LIMIT 1");
 
-    if ($password === $password_confirm)
+    $number = preg_match('@[0-9]@', $password);
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
+    if(strlen($password) < 4 || !$number || !$uppercase || !$lowercase || !$specialChars)
     {
-        if (mysqli_num_rows($sql) === 1)
+        $_SESSION['message'] = "password must be at least 8 characters in length and must contain at least one number, one upper case letter, one lower case letter and one special character";
+        header('Location: ../signup.php');
+    }
+    else
+    {
+        if ($password === $password_confirm)
         {
-
-            $user = mysqli_fetch_assoc($sql);
-            if(password_verify($password_old, $user['password']))
+            if (mysqli_num_rows($sql) === 1)
             {
 
-                $new_password = password_hash($password, PASSWORD_BCRYPT);
-
-                $update = mysqli_query($connect,"UPDATE `users` SET  `password` = '$new_password' WHERE `login` = '$login' LIMIT 1");
-                if($update)
+                $user = mysqli_fetch_assoc($sql);
+                if(password_verify($password_old, $user['password']))
                 {
-                    $_SESSION['message'] = 'password changed successfully';
-                    header('Location: ../profile.php');
+
+                    $new_password = password_hash($password, PASSWORD_BCRYPT);
+
+                    $update = mysqli_query($connect,"UPDATE `users` SET  `password` = '$new_password' WHERE `login` = '$login' LIMIT 1");
+                    if($update)
+                    {
+                        $_SESSION['message'] = 'password changed successfully, your password is strong';
+                        header('Location: ../profile.php');
+                    }
+                    else
+                    {
+                        $_SESSION['message'] = 'error in data base';
+                        header('Location: ../upd.php');
+                    }
+
                 }
                 else
                 {
-                    $_SESSION['message'] = 'error in data base';
+                    $_SESSION['message'] = 'wrong old password';
                     header('Location: ../upd.php');
                 }
 
             }
             else
             {
-                $_SESSION['message'] = 'wrong old password';
+                $_SESSION['message'] = 'wrong username';
                 header('Location: ../upd.php');
             }
 
         }
         else
         {
-            $_SESSION['message'] = 'wrong username';
+            $_SESSION['message'] = "passwords don't match";
             header('Location: ../upd.php');
         }
-
-    }
-    else
-    {
-        $_SESSION['message'] = "passwords don't match";
-        header('Location: ../upd.php');
     }
